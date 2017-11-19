14f8d68ca4e71b52bdfd0a2c0d9ee3149bc98443
Lukas <seriouz.accz@gmail.com>
2017-11-19 10:12:45 +0100

    Finish chapter 2

--- src/clojuring/chapter_2.clj
+++ src/clojuring/chapter_2.clj
@@ -20,13 +20,78 @@
     {}
     map))
 
-(defn item
-  [i]
-  (println "creating an item " i)
-  {:name "an item"})
+(def filename "ch2-suspects.csv")
+(def vamp-keys [:name :glitter-index])
 
-(def items (map item (range 100)))
+(defn str->int
+  [str]
+  (Integer. str))
+
+(def conversions {:name          identity
+                  :glitter-index str->int})
+
+(defn convert
+  [vamp-key value]
+  ((get conversions vamp-key) value))
+
+(defn parse
+  "Convert a CSV into rows of columns"
+  [string]
+  (map #(clojure.string/split % #",")
+       (clojure.string/split string #"\n")))
+
+(defn mapify
+  "Return a seq of maps like {:name \"Edward Cullen\" :glitter-index 10}"
+  [rows]
+  (map (fn [unmapped-row]
+         (reduce (fn [row-map [vamp-key value]]
+                   (assoc row-map vamp-key (convert vamp-key value)))
+                 {}
+                 (map vector vamp-keys unmapped-row)))
+       rows))
+
+(defn glitter-filter
+  [minimum-glitter records]
+  (filter #(>= (:glitter-index %) minimum-glitter) records))
+
+(def vamp-db (mapify (parse (slurp filename))))
+
+(defn filter-vamps
+  [vamp-db]
+  (glitter-filter 3 vamp-db))
+
+(defn names-only [vam-db]
+  (map #(get % :name) (filter-vamps vamp-db)))
+
+(defn append
+  [db record]
+  (conj db record))
+
+(defn to-csv-row
+  [{:keys [name glitter-index]}]
+  (clojure.string/join "," [name glitter-index]))
+
+
+(defn valid?
+  [keys rec]
+  (reduce #(and %1 (not (nil? (rec %2))))
+          true
+          keys))
+
+
+(defn to-csv
+  [db]
+  (clojure.string/join "\n"
+                       (map (to-csv-row vamp-keys) db)))
+
+(defn to-csv-row
+  [keys]
+  (fn [rec]
+    (clojure.string/join ","
+                         (map #(rec %) keys))))
 
 (defn go []
-  (println "going")
-  (concat (take 33 items) (take 33 items)))
+  (let [rec {:name "Jano" :glitter-index 1234}]
+    (if (not (valid? vamp-keys rec))
+      "Invalid record"
+      (to-csv (append vamp-db rec)))))
